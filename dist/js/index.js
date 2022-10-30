67b1de15e3d5c1ad455bb6b4adb62ccef80438de
Mikhail Tyapkov <andrimalano3@gmail.com>
2022-10-30 22:30:35 +0300

    2-4(1/2) blocks

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,5 +2,15 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
+
+$(document).ready(function () {
+  let $page = $('html, body');
+  $('a[href*="#"]').click(function () {
+    $page.animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+    return false;
+  });
+});
 /******/ })()
 ;
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+$(document).ready(function(){
+
+    let $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+        return false;
+    });
+
+});
